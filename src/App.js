e8a336d10b272b7cf22b3817267d0f618e7d84b2
Umut <umut@dummy.com>
2022-05-03 18:38:28 +0300

    empty board and keyboard components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <h1>Twordle</h1>
+      </nav>
+      <Board />
+      <Keyboard />
     </div>
   );
 }
